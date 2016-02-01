0448bf53d34150ebedd975475fc9bfae5cf0f353
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-01 19:18:02 +0100

    Tests for mean function

--- mean_test.go
+++ mean_test.go
@@ -1,7 +1,17 @@
 package statistics
 
 import "testing"
+import "strconv"
 
 func TestMean(t *testing.T) {
+	list := []float64{1, 2, 3}
+	if mean, _ := Mean(list); mean != 2 {
+		t.Error("The mean must be 2 no: "+
+			strconv.FormatFloat(mean, 'f', 2, 64),
+		)
+	}
 
+	if mean, err := Mean([]float64{}); mean != 0.0 && err != nil {
+		t.Error(err)
+	} 
 }
