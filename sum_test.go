c9af2d15846a8ea246ecca4a192b7ff95e72ff61
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-01 16:30:50 +0100

    Test for Sum function

--- sum_test.go
+++ sum_test.go
@@ -1,1 +1,11 @@
-sum_test
+package statistics
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	list := []float64{1.2, 2, 3}
+	result := Sum(list)
+	if result != 6.2 {
+		t.Error("Sum result must be equal to 6.2")
+	}
+}
