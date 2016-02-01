35eb6f5fdb4a92d8cda44f7f3e17acd4b2c90bf1
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-01 19:32:25 +0100

    Documentation for max function

--- max.go
+++ max.go
@@ -2,6 +2,8 @@ package statistics
 
 import "errors"
 
+// This function obtain the maximum number in a slice
+// or returns and error if the list is empty.
 func Max(list []float64) (float64, error) {
 	if len(list) == 0 {
 		return 0.0, errors.New("List is empty")
