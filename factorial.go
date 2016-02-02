d57a66dd50b33a138cf87a8a94e283c827eca739
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-02 10:51:57 +0100

    Added factorial function

--- factorial.go
+++ factorial.go
@@ -1,1 +1,20 @@
-poisson_distribution
+package statistics
+
+import "errors"
+
+func Factorial(n int) (int, error) {
+	if n < 0 {
+		return 0, errors.New("Less than 0")
+	}
+
+	if n == 0 {
+		return 1, nil
+	}
+
+	total := 1
+	for i := 1; i <= n; i++ {
+		total *= i
+	}
+
+	return total, nil
+}
