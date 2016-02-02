a69e4b85ff5a409997c25a77db726f735b73a4b9
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-02 11:39:23 +0100

    Documentation for factorial function

--- factorial.go
+++ factorial.go
@@ -2,6 +2,9 @@ package statistics
 
 import "errors"
 
+// calculate the factorial of a given number.
+// if less than 0 then return an error and by default
+// factorial of 0 is 1.
 func Factorial(n int) (int, error) {
 	if n < 0 {
 		return 0, errors.New("Less than 0")
