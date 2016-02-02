7cdfde8c2eb71dc7f531c5acd932799ed2a44f08
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-03 00:22:47 +0100

    tests for quadratic mean

--- quadratic_mean.go
+++ quadratic_mean.go
@@ -3,7 +3,8 @@ package statistics
 import "errors"
 import "math"
 
-//
+// calculates the quadratic mean and throws an error
+// if the list is empty.
 func QuadraticMean(list []float64) (float64, error) {
 	if len(list) == 0 {
 		return 0.0, errors.New("Empty list")
--- quadratic_mean_test.go
+++ quadratic_mean_test.go
@@ -1,7 +1,21 @@
 package statistics
 
-import "testing"
+import(
+	"testing"
+	"strconv"
+)
 
 func TestQuadraticMean(t *testing.T) {
-	
+	if mean, _ := QuadraticMean([]float64{-1, 1, -1, 1}); mean != 1 {
+		t.Error("Quadratic mean should return 1")
+	}
+
+	if mean, _ := QuadraticMean([]float64{3, 4, 5}); strconv.FormatFloat(mean, 'f', 3, 64) != "4.082" {
+		t.Error("Quadratic mean should return 4.082")
+	}
+
+
+	if _, err := QuadraticMean([]float64{}); err == nil {
+		t.Error("Quadratic mean should recieve a slice whit length higher than 0")
+	}
 }
