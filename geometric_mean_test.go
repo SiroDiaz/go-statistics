d111cabc3163235d912f8a59e86c359eb0216361
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-04 00:30:04 +0100

    tests for geometric mean

--- geometric_mean_test.go
+++ geometric_mean_test.go
@@ -1,2 +1,20 @@
 package statistics
 
+import(
+	"testing"
+	"strconv"
+)
+
+func TestGeometricMean(t *testing.T) {
+	if gm, _ := GeometricMean([]float64{2, 8}); gm != 4 {
+		t.Error("The geometric mean must be equal to 4")
+	}
+	
+	if gm, _ := GeometricMean([]float64{4, 1, 1.0 / 32}); strconv.FormatFloat(gm, 'g', 1, 64) != "0.5" {
+		t.Error("The geometric mean must be equal to 0.5")
+	}
+
+	if _, err := GeometricMean([]float64{}); err == nil {
+		t.Error("Empty list for geometric mean should return an error")
+	}
+}
