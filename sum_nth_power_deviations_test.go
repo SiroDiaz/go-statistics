3b5eca62b1db63ad1342deae236ab1e5e5e18f34
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-04 12:41:45 +0100

    Tests for sum nth power deviation

--- sum_nth_power_deviations_test.go
+++ sum_nth_power_deviations_test.go
@@ -1,2 +1,19 @@
 package statistics
 
+import(
+	"testing"
+)
+
+func TestSumNthPowerDeviations(t *testing.T) {
+	if tspd, _ := SumNthPowerDeviations([]float64{0,0,0}, 2); tspd != 0 {
+		t.Error("Sum nth power deviation must be equal to 0")
+	}
+
+	if tspd, _ := SumNthPowerDeviations([]float64{0,1,2}, 2); tspd != 2 {
+		t.Error("Sum nth power deviation must be equal to 2")
+	}
+
+	if _, err := SumNthPowerDeviations([]float64{}, 2); err == nil {
+		t.Error("Empty list")
+	}
+}
